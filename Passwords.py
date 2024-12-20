9ae8054489ec04ff3fe3ca60fc4184273694e1f4
Dmitry <nikolaev.dme@gmail.com>
2024-12-20 17:46:24 +0300

    Install

--- Passwords.py
+++ Passwords.py
@@ -3,5 +3,5 @@ import string
 
 def generate_unique_codes():
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(8))
-    return password
+    password1 = ''.join(random.choice(characters) for _ in range(8))
+    return password1
